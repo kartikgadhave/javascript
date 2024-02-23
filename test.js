1ccb5ed1de377dfd3a9f53e3c5b509f9913ac1c3
Kartik gadhave <kgadhave71@gmail.com>
2024-02-23 22:20:39 +0530

    push

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-console.log("Kartik")
-console.log("kp")
-let a=20
-let b=30
+const accountId=12543;
+let email="kgadhave71@gmil.com"
+var city="mumbai";
+
+//console.table([accountId,email,city])
+/*
+let score="Kartik"
+let Numberconvert=Number(score)
+console.log(Numberconvert)
+console.log(typeof Numberconvert)*/
+
+let name="sipna"
+let convert=Boolean(name)
+console.log( convert)
+console.log(typeof convert)
