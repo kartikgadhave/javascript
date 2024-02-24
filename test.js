0442e2dd704920e805295ddb07efb317612f7ac2
Kartik gadhave <kgadhave71@gmail.com>
2024-02-24 22:37:31 +0530

    push

--- test.js
+++ test.js
@@ -9,7 +9,35 @@ let Numberconvert=Number(score)
 console.log(Numberconvert)
 console.log(typeof Numberconvert)*/
 
-let name="sipna"
-let convert=Boolean(name)
-console.log( convert)
-console.log(typeof convert)
+//let name="sipna"
+//let convert=Boolean(name)
+//onsole.log( convert)
+//console.log(typeof convert)
+
+//console.log(null>0)
+//console.log(null<=0)//
+
+const bigInt=25746547434844n
+console.log(typeof bigInt)
+
+let string=["kartik",23,25,"ayan"];
+console.log(typeof string)
+
+let obj={
+    name:"gadhave",
+    age:26,
+    college:"sipnacoet",
+
+}
+console.log(typeof obj)
+
+const id=Symbol('100')
+console.log(typeof id)
+
+const gameName = new String("Sipna College Of engineering And Technlogy Amravati")
+
+console.log(gameName.length)
+console.log(gameName.charAt(10))
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
+console.log(gameName.indexOf('l'))
